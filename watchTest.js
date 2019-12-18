f3a1c04edf215c23bdedb4306ecd2cd03420e91e
Jaid <jaid.jsx@gmail.com>
2019-12-18 15:55:20 +0100

    Reverted debugging changes
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- watchTest.js
+++
@@ -1,7 +0,0 @@
-import chokidar from "chokidar"
-
-chokidar.watch(".", {
-  cwd: __dirname,
-}).on("all", (event, path) => {
-  console.log(`${event} ${path}`)
-})
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,33 +1,5 @@
-import p from "log-watcher-webpack-plugin"
 import configure from "webpack-config-jaid"
-/**
- * @type {import("webpack").Configuration}
- */
-const extra = {
-  plugins: [new p],
-  devServer: {
-    noInfo: false,
-    stats: "verbose",
-  },
-  watch: false,
-  watchOptions: {
-    ignored: "**",
-  },
-}
 
-const config = configure({
-  extra,
+export default configure({
   configOutput: true,
 })
-
-/**
- * @type {import("webpack-dev-server").Configuration}
- */
-config.devServer = {
-  port: 1212,
-  overlay: true,
-  // hot: true,
-  watchContentBase: false,
-}
-
-export default config
