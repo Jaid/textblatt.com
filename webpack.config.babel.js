da43e01e8d81d1bf6e5cd761c085498cfc3f88eb
Jaid <jaid.jsx@gmail.com>
2019-12-27 08:55:14 +0100

    Simplified Webpack config
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,5 +3,5 @@ import configure from "webpack-config-jaid"
 export default configure({
   googleAnalyticsTrackingId: "UA-154709538-2",
   twitterAuthorHandle: "jaidchen",
-  themeColor: "#00baff",
+  themeColor: "00baff",
 })
