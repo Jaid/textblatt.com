405969c34420e43395af8257966f9cf78692c232
Jaid <jaid.jsx@gmail.com>
2021-06-21 16:31:06 +0200

    Removed dependency react-hot-loader

--- src/components/HotApp/index.js
+++
@@ -1,5 +0,0 @@
-import {hot} from "react-hot-loader"
-
-import App from "src/components/App"
-
-export default hot(module)(App)
--- src/index.development.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDom from "react-dom"
-
-import HotApp from "src/components/HotApp"
-
-const rootNode = document.querySelector("body>div")
-ReactDom.render(<HotApp/>, rootNode)
