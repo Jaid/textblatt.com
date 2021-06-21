2a02c8594952a4b243098c107200d4304f2f3dd8
Jaid <jaid.jsx@gmail.com>
2021-06-21 18:39:51 +0200

    Removed offline-plugin
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDom from "react-dom"
 
 import App from "src/components/App"
 
-require("@lcdp/offline-plugin/runtime").install()
-
 const rootNode = document.querySelector("body>div")
 ReactDom.render(<App/>, rootNode)
