898630baf33ae2c2c318482dbf18e1e2f7541b8f
Jaid <jaid.jsx@gmail.com>
2021-06-21 18:37:31 +0200

    Upgraded dependencies

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import App from "src/components/App"
 require("@lcdp/offline-plugin/runtime").install()
 
 const rootNode = document.querySelector("body>div")
-// ReactDom.render(<App/>, rootNode)
+ReactDom.render(<App/>, rootNode)
